841ea77e890b769de90457ec1b0972fc3e908b50
Mads Milo <madsmilo159@gmail.com>
2026-05-09 13:22:15 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,63 @@
+/*
+Simple C Calculator
+Made by https://github.com/Mads-Milo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Addition
+void add(int a, int b)
+{
+    printf("%d", a + b);
+}
+
+// Subtraction
+void subtract(int a, int b)
+{
+    printf("%d", a - b);
+}
+
+// Mulitiplication
+void multiply(int a, int b)
+{
+    printf("%d", a * b);
+}
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int operator = 1;
+
+    // Get numbers
+    printf("Enter first number >> ");
+    scanf("%d", &a);
+    printf("Enter second number >> ");
+    scanf("%d", &b);
+
+    // Get Operator
+    printf("[1] Addition\n");
+    printf("[2] Subtraction\n");
+    printf("[3] Multiplication\n");
+    printf("Select Operator >> ");
+    scanf("%d", &operator);
+
+    // Check Operator
+    if(operator==1)
+    {
+        add(a, b);
+    }
+    else if(operator==2)
+    {
+        subtract(a, b);
+    }
+    else if(operator==3)
+    {
+        multiply(a, b);
+    }
+
+    system("pause");
+
+    return 0;
+}
